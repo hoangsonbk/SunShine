2cc9aeec7e1cc61b17503192231cb1314633d1dc
Son Nguyen <hoangsontemp@gmail.com>
2015-01-07 18:39:36 +0700

    add Loaders and CursorAdapter

--- src/gemmy/sunshine/ForecastFragment.java
+++ src/gemmy/sunshine/ForecastFragment.java
@@ -1,5 +1,9 @@
 package gemmy.sunshine;
 
+import gemmy.sunshine.data.WeatherContract;
+import gemmy.sunshine.data.WeatherContract.LocationEntry;
+import gemmy.sunshine.data.WeatherContract.WeatherEntry;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,6 +12,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import org.json.JSONException;
@@ -16,11 +21,16 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.util.Log;
@@ -38,25 +48,62 @@ import android.widget.Toast;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class ForecastFragment extends Fragment {
+public class ForecastFragment extends Fragment implements LoaderCallbacks<Cursor> {
 
-	private ArrayAdapter<String> mForecastAdapter;
+	private SimpleCursorAdapter mForecastAdapter;
 	private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+	
+    private static final int FORECAST_LOADER = 0;
+    private String mLocation;
+
+    // For the forecast view we're showing only a small subset of the stored data.
+    // Specify the columns we need.
+    private static final String[] FORECAST_COLUMNS = {
+            // In this case the id needs to be fully qualified with a table name, since
+            // the content provider joins the location & weather tables in the background
+            // (both have an _id column)
+            // On the one hand, that's annoying.  On the other, you can search the weather table
+            // using the location set by the user, which is only in the Location table.
+            // So the convenience is worth it.
+            WeatherEntry.TABLE_NAME + "." + WeatherEntry._ID,
+            WeatherEntry.COLUMN_DATETEXT,
+            WeatherEntry.COLUMN_SHORT_DESC,
+            WeatherEntry.COLUMN_MAX_TEMP,
+            WeatherEntry.COLUMN_MIN_TEMP,
+            LocationEntry.COLUMN_LOCATION_SETTING
+    };
+
+
+    // These indices are tied to FORECAST_COLUMNS.  If FORECAST_COLUMNS changes, these
+    // must change.
+    public static final int COL_WEATHER_ID = 0;
+    public static final int COL_WEATHER_DATE = 1;
+    public static final int COL_WEATHER_DESC = 2;
+    public static final int COL_WEATHER_MAX_TEMP = 3;
+    public static final int COL_WEATHER_MIN_TEMP = 4;
+    public static final int COL_LOCATION_SETTING = 5;
+
 	
 	public ForecastFragment() {
 	}
 	
 	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		getLoaderManager().initLoader(FORECAST_LOADER, null, this);
+	    super.onActivityCreated(savedInstanceState);
+	    }
+	
+	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setHasOptionsMenu(true);
-	}
+		}
 	
 	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		inflater.inflate(R.menu.forecastfragment, menu);
-	}
+		}
 	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -67,18 +114,15 @@ public class ForecastFragment extends Fragment {
 		if (id == R.id.action_refresh) {
 			updateWeather();
 			return true;
-		}
+			}
 		return super.onOptionsItemSelected(item);
 	}
 	
 	private void updateWeather(){
-		FetchWeatherTask weatherTask = new FetchWeatherTask();
-		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
-		String location = prefs.getString(getString(R.string.pref_location_key), "Ho chi minh");
+		FetchWeatherTask weatherTask = new FetchWeatherTask(getActivity());
+		String location = Utility.getPreferredLocation(getActivity());
 		weatherTask.execute(location);
 	}
-	
-	
 	
 	@Override
 	public void onStart(){
@@ -103,11 +147,32 @@ public class ForecastFragment extends Fragment {
 				"Sun - Sunny - 27/29"
 		};
 		List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
-		mForecastAdapter = new ArrayAdapter<String>(
-				context, //Current context, this fragment's parent activity
+		mForecastAdapter = new SimpleCursorAdapter(
+                getActivity(),
+                R.layout.list_item_forecast,
+                null,
+                // the column names to use to fill the textviews
+                new String[]{WeatherContract.WeatherEntry.COLUMN_DATETEXT,
+                        WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
+                        WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+                        WeatherContract.WeatherEntry.COLUMN_MIN_TEMP
+                },
+                // the textviews to fill with the data pulled from the columns above
+                new int[]{R.id.list_item_date_textview,
+                        R.id.list_item_forecast_textview,
+                        R.id.list_item_high_textview,
+                        R.id.list_item_low_textview
+                },
+                0
+        );
+		
+		/* OLD CODE USING ARRAY ADAPTER
+		 mForecastAdapter = new ArrayAdapter<String>(
+				getActivity(),
 				R.layout.list_item_forecast,
 				R.id.list_item_forecast_textview,
-				weekForecast);
+				weekForecast);*/
+		
 		ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
 		listView.setAdapter(mForecastAdapter);
 		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -115,18 +180,61 @@ public class ForecastFragment extends Fragment {
 			@Override
 			public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
 				// TODO Auto-generated method stub
-				String forecast = mForecastAdapter.getItem(position);
+				/* OLD CODE USING ARRAY ADAPTER
+			    String forecast = mForecastAdapter.getItem(position);
 				Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
 				Intent intentToDetailAct = new Intent (getActivity(), DetailActivity.class);
 				intentToDetailAct.putExtra(Intent.EXTRA_TEXT, forecast);
-				startActivity(intentToDetailAct);
+				startActivity(intentToDetailAct);*/
 			}
 			
 		});
 		return rootView;
 	}
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // This is called when a new Loader needs to be created.  This
+        // fragment only uses one loader, so we don't care about checking the id.
+
+        // To only show current and future dates, get the String representation for today,
+        // and filter the query to return weather only for dates after or including today.
+        // Only return data after today.
+        String startDate = WeatherContract.getDbDateString(new Date());
+
+        // Sort order:  Ascending, by date.
+        String sortOrder = WeatherEntry.COLUMN_DATETEXT + " ASC";
+
+        mLocation = Utility.getPreferredLocation(getActivity());
+        Uri weatherForLocationUri = WeatherEntry.buildWeatherLocationWithStartDate(
+                mLocation, startDate);
+
+        // Now create and return a CursorLoader that will take care of
+        // creating a Cursor for the data being displayed.
+        return new CursorLoader(
+                getActivity(),
+                weatherForLocationUri,
+                FORECAST_COLUMNS,
+                null,
+                null,
+                sortOrder
+        );
+    }
+	@Override
+	public void onLoadFinished(Loader<Cursor> arg0, Cursor data) {
+		// TODO Auto-generated method stub
+		mForecastAdapter.swapCursor(data);
+		
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> arg0) {
+		// TODO Auto-generated method stub
+		mForecastAdapter.swapCursor(null);
+		
+	}
 	
-	@SuppressLint("NewApi")
+	/*@SuppressLint("NewApi")
 	public class FetchWeatherTask extends AsyncTask<String, Void, String[]>{		
 		@Override
 		protected String[] doInBackground(String...param) {
@@ -225,5 +333,5 @@ public class ForecastFragment extends Fragment {
 			mForecastAdapter.notifyDataSetChanged();
 	     }
 		
-	}
+	}*/
 }
